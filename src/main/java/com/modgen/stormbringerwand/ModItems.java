9330e264ed61efe645df198e86ed4132afc8d620
matfaz <153545694+matfaz@users.noreply.github.com>
2025-12-21 02:27:06 +0000

    build(modgen): Stormbringer Wand (3b31e924-8cce-4b97-8bc2-22bdfcd29779)

---
+++ src/main/java/com/modgen/stormbringerwand/ModItems.java
@@ -0,0 +1,30 @@
+package com.modgen.stormbringerwand;
+
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+
+import com.modgen.stormbringerwand.item.LightningWandItem;
+
+public class ModItems {
+    public static final Item LIGHTNING_WAND = registerItem("lightning_wand", new LightningWandItem(new Item.Settings().maxCount(1).maxDamage(250).rarity(Rarity.RARE)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(StormbringerWand.MOD_ID, name), item);
+    }
+
+    public static void registerItems() {
+        StormbringerWand.LOGGER.info("Registering items for " + StormbringerWand.MOD_ID);
+    }
+    
+    public static Item getFirstItem() {
+        return LIGHTNING_WAND;
+    }
+    
+    public static void addToCreativeTab(ItemGroup.Entries entries) {
+        entries.add(LIGHTNING_WAND);
+    }
+}
