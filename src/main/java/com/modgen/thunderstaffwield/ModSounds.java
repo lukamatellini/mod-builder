82b02bc797bc75349f3dc3885748239007bc049c
matfaz <153545694+matfaz@users.noreply.github.com>
2025-12-21 01:52:32 +0000

    build(modgen): Thunderstaff Wield (2f9be62d-8473-49a2-ab56-9dd4686ef81e)

---
+++ src/main/java/com/modgen/thunderstaffwield/ModSounds.java
@@ -0,0 +1,21 @@
+package com.modgen.thunderstaffwield;
+
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
+
+public class ModSounds {
+    public static final SoundEvent UI_CLICK = registerSound("ui_click");
+    public static final SoundEvent QUEST_COMPLETE = registerSound("quest_complete");
+    public static final SoundEvent NPC_GREET = registerSound("npc_greet");
+    
+    private static SoundEvent registerSound(String name) {
+        Identifier id = Identifier.of(ThunderstaffWield.MOD_ID, name);
+        return Registry.register(Registries.SOUND_EVENT, id, SoundEvent.of(id));
+    }
+    
+    public static void registerSounds() {
+        ThunderstaffWield.LOGGER.info("Registered sounds for " + ThunderstaffWield.MOD_ID);
+    }
+}
