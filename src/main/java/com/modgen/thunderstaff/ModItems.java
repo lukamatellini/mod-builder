f1aba9f4166edd38352b3997d55f17ea94fa014c
matfaz <153545694+matfaz@users.noreply.github.com>
2025-12-21 23:41:36 +0000

    build(modgen): Thunder Staff (d50b22e4-1cc0-4553-906e-ad3e78f55d6a)

---
+++ src/main/java/com/modgen/thunderstaff/ModItems.java
@@ -0,0 +1,30 @@
+package com.modgen.thunderstaff;
+
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+
+import com.modgen.thunderstaff.item.LightningWandItem;
+
+public class ModItems {
+    public static final Item LIGHTNING_WAND = registerItem("lightning_wand", new LightningWandItem(new Item.Settings().maxCount(1).maxDamage(128).rarity(Rarity.UNCOMMON)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(ThunderStaff.MOD_ID, name), item);
+    }
+
+    public static void registerItems() {
+        ThunderStaff.LOGGER.info("Registering items for " + ThunderStaff.MOD_ID);
+    }
+    
+    public static Item getFirstItem() {
+        return LIGHTNING_WAND;
+    }
+    
+    public static void addToCreativeTab(ItemGroup.Entries entries) {
+        entries.add(LIGHTNING_WAND);
+    }
+}
