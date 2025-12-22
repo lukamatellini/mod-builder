2121af8e86d2c1e27fd6a23d8cede1df43191d49
matfaz <153545694+matfaz@users.noreply.github.com>
2025-12-22 21:18:56 +0000

    build(modgen): Thunder Rods (fa980135-698a-4d2e-aeaf-f17d795bd287)

---
+++ src/client/java/com/modgen/thunderrods/client/ThunderRodsClient.java
@@ -0,0 +1,20 @@
+package com.modgen.thunderrods.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Minimal client entrypoint
+ */
+@Environment(EnvType.CLIENT)
+public class ThunderRodsClient implements ClientModInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger("thunderrods/client");
+    
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("Thunder Rods client initialized");
+    }
+}
