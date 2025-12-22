0ef1a307016eb96c89c3161749450ecf570d2dbc
matfaz <153545694+matfaz@users.noreply.github.com>
2025-12-22 21:21:18 +0000

    build(modgen): Cheesy Blocks (5d4893a3-fec0-4f9f-9ab5-89b975f65640)

---
+++ src/main/java/com/modgen/cheesyblocks/structure/CheeseHouseStructure.java
@@ -0,0 +1,8 @@
+package com.modgen.cheesyblocks.structure;
+
+import com.modgen.cheesyblocks.CheesyBlocks;
+
+public class CheeseHouseStructure {
+    public static final String ID = "cheese_house";
+    public static final String NAME = "Cheese House";
+}
