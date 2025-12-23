42c5848d5b40795a68fe3def17a9c458c265296f
matfaz <153545694+matfaz@users.noreply.github.com>
2025-12-23 04:32:37 +0000

    build(modgen): Swiftstride Steps (00f2b8fa-ebe7-44ff-ae1f-ca83dcd5dc0d)

---
+++ src/main/java/com/modgen/swiftstridesteps/structure/ParrotNestStructure.java
@@ -0,0 +1,8 @@
+package com.modgen.swiftstridesteps.structure;
+
+import com.modgen.swiftstridesteps.SwiftstrideSteps;
+
+public class ParrotNestStructure {
+    public static final String ID = "parrot_nest";
+    public static final String NAME = "Parrot Nest";
+}
