9fe44a359a0758de50e104794131745a9c901558
matfaz <153545694+matfaz@users.noreply.github.com>
2025-12-23 02:44:36 +0000

    build(modgen): Skybolt Wand (f3581c0f-3976-4680-b9c3-e93835ec8a69)

---
+++ src/client/java/com/modgen/skyboltwand/client/SkyboltWandClient.java
@@ -0,0 +1,20 @@
+package com.modgen.skyboltwand.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Minimal client entrypoint
+ */
+@Environment(EnvType.CLIENT)
+public class SkyboltWandClient implements ClientModInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger("skyboltwand/client");
+    
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("Skybolt Wand client initialized");
+    }
+}
