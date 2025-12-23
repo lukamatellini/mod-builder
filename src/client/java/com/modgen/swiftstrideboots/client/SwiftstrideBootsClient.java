a5f5c0eb9bc918be45a42e4063a111d610047f8a
matfaz <153545694+matfaz@users.noreply.github.com>
2025-12-23 00:45:49 +0000

    build(modgen): SwiftStride Boots (5926d33f-4eed-4103-8729-30acc519cd78)

---
+++ src/client/java/com/modgen/swiftstrideboots/client/SwiftstrideBootsClient.java
@@ -0,0 +1,20 @@
+package com.modgen.swiftstrideboots.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Minimal client entrypoint
+ */
+@Environment(EnvType.CLIENT)
+public class SwiftstrideBootsClient implements ClientModInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger("swiftstrideboots/client");
+    
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("SwiftStride Boots client initialized");
+    }
+}
