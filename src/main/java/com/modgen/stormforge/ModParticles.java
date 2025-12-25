c23291b5e3a682d5d1be57d90b70df67ce859889
matfaz <153545694+matfaz@users.noreply.github.com>
2025-12-25 17:16:20 +0000

    build(modgen): Storm Forge (3e64aa27-8f0b-4b29-85a2-efa1d6099b23)

---
+++ src/main/java/com/modgen/stormforge/ModParticles.java
@@ -0,0 +1,23 @@
+package com.modgen.stormforge;
+
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.particle.SimpleParticleType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+/**
+ * Mod Particle Types Registry
+ * Custom particles for abilities, portals, and effects
+ */
+public class ModParticles {
+    
+    public static final SimpleParticleType FLAME = FabricParticleTypes.simple();
+    
+    /**
+     * Register all particles
+     */
+    public static void register() {
+        Registry.register(Registries.PARTICLE_TYPE, Identifier.of(StormForge.MOD_ID, "flame"), FLAME);
+    }
+}
