de31a986d35dcb3bf62c934869f95b17c94b0696
matfaz <153545694+matfaz@users.noreply.github.com>
2025-12-26 19:57:19 +0000

    build(modgen): Lightning Wand (fc0375bc-7d5d-4142-b2f1-bcb1b8b2ff82)

---
+++ src/client/java/com/modgen/lightningwand/client/LightningWandClient.java
@@ -0,0 +1,20 @@
+package com.modgen.lightningwand.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Minimal client entrypoint
+ */
+@Environment(EnvType.CLIENT)
+public class LightningWandClient implements ClientModInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger("lightningwand/client");
+    
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("Lightning Wand client initialized");
+    }
+}
