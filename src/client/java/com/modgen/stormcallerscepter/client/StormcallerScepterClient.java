3a53f749e7ed68b36a49fd52c0bfbadbbc9ea08b
matfaz <153545694+matfaz@users.noreply.github.com>
2025-12-26 17:14:35 +0000

    build(modgen): Stormcaller Scepter (af95f350-77ad-44a1-b87a-1d408062edb9)

---
+++ src/client/java/com/modgen/stormcallerscepter/client/StormcallerScepterClient.java
@@ -0,0 +1,20 @@
+package com.modgen.stormcallerscepter.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Minimal client entrypoint
+ */
+@Environment(EnvType.CLIENT)
+public class StormcallerScepterClient implements ClientModInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger("stormcallerscepter/client");
+    
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("Stormcaller Scepter client initialized");
+    }
+}
