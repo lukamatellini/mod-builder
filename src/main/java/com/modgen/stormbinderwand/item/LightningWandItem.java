94b31a8cac7fabbd62f7f0b568112269d53ee4a7
matfaz <153545694+matfaz@users.noreply.github.com>
2025-12-27 13:29:58 +0000

    build(modgen): Stormbinder Wand (f398a6d3-78fc-4a72-ac97-1e57defa1e7c)

---
+++ src/main/java/com/modgen/stormbinderwand/item/LightningWandItem.java
@@ -0,0 +1,64 @@
+package com.modgen.stormbinderwand.item;
+
+import com.modgen.stormbinderwand.StormbinderWand;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.SmallFireballEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+
+import java.util.List;
+
+public class LightningWandItem extends Item {
+    
+    public LightningWandItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+        ItemStack stack = player.getStackInHand(hand);
+        
+        // Play sound using vanilla SoundEvents
+        world.playSound(null, player.getX(), player.getY(), player.getZ(), 
+            SoundEvents.ENTITY_BLAZE_SHOOT, SoundCategory.PLAYERS, 1.0f, 1.0f);
+        
+        if (!world.isClient) {
+            // Strike lightning bolt at target (Yarn 1.21.1 compatible)
+            if (world instanceof ServerWorld serverWorld) {
+                var hit = player.raycast(50.0, 0, false);
+                BlockPos targetPos = BlockPos.ofFloored(hit.getPos());
+                EntityType.LIGHTNING_BOLT.spawn(serverWorld, targetPos, SpawnReason.TRIGGERED);
+            }
+        }
+        
+        // Cooldown (1.21.1 API - uses stack.getItem() instead of stack)
+        player.getItemCooldownManager().set(stack.getItem(), 20);
+        
+        // Durability damage if applicable
+        stack.damage(1, player, LivingEntity.getSlotForHand(hand));
+        
+        return TypedActionResult.success(stack, world.isClient());
+    }
+
+    
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.literal("§7Right-click to use"));
+
+    }
+}
